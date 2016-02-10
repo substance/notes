47921b1964c08dc3b9d58fe175df63893a5901a3
Michael Aufreiter <michael@substance.io>
2016-02-10 18:18:48 +0100

    Basic server setup (WIP)

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var path = require('path');
 var app = express();
 var port = process.env.PORT || 5000;
 var server = require('substance/util/server');
-
-// var hub = require('substance-hub');
+var Hub = require('substance/util/StubHub');
+var TestStore = require('substance/util/TestStore');
+var http = require('http');
+var WebSocketServer = require('ws').Server;
+var exampleNoteChangeset = require('./note/exampleNoteChangeset');
 
 // Serve app in development mode
 // ----------------
@@ -15,10 +18,24 @@ server.serveJS(app, '/app.js', path.join(__dirname, 'notepad', 'app.js'));
 app.use(express.static(path.join(__dirname, 'notepad')));
 app.use('/fonts', express.static(path.join(__dirname, 'node_modules/font-awesome/fonts')));
 
-// // Connect Substance realtime hub
+
+// Connect Substance
 // ----------------
 
-hub.connect(app, port);
+var httpServer = http.createServer();
+var wss = new WebSocketServer({ server: httpServer });
+
+// Will be replaced with Daniel's persistent store
+var store = new TestStore({
+  'note-1': exampleNoteChangeset()
+});
+
+var hub = new Hub(wss, store);
+
+// Delegate http requests to express app
+server.on('request', app);
+
+server.listen(port, function() { console.log('Listening on ' + server.address().port); });
 
 // Export app for requiring in test files
 module.exports = app;
