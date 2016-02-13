b3d0ff2569c74be1334d30e3d0421a7c005b6662
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-02-13 14:59:30 +0100

    Provide configuration from env variables into client.

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var port = process.env.PORT || 5000;
+var host = process.env.HOST || 'localhost';
 var server = require('substance/util/server');
 var CollabHub = require('substance/util/CollabHub');
 var Storage = require('./hub/ChangesStore');
 var http = require('http');
 var WebSocketServer = require('ws').Server;
-var exampleNoteChangeset = require('./note/exampleNoteChangeset');
 
 var knexConfig = require('./knexfile');
 
@@ -16,6 +16,8 @@ var knexConfig = require('./knexfile');
 
 server.serveStyles(app, '/app.css', path.join(__dirname, 'notepad', 'app.scss'));
 server.serveJS(app, '/app.js', path.join(__dirname, 'notepad', 'app.js'));
+var config = { port: port, host: host };
+server.serveHTML(app, '/', path.join(__dirname, 'notepad', 'index.html'), config);
 
 app.use(express.static(path.join(__dirname, 'notepad')));
 app.use('/fonts', express.static(path.join(__dirname, 'node_modules/font-awesome/fonts')));
