2d01f41fbee93534288bb97f906813e08402b68d
Michael Aufreiter <michael@substance.io>
2016-02-29 18:53:59 +0100

    Setup test environment.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('lint', function() {
+  return gulp.src([
+    './model/**/*.js',
+    './client/**/*.js',
+    './server/**/*.js',
+  ]).pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter("fail"));
+});
+
+
+gulp.task('test:server', ['lint'], function() {
+  // requiring instead of doing 'node test/run.js'
+  // require('./test/run');
+});
+
+gulp.task('test', ['lint', 'test:server']);
+gulp.task('default', ['build']);
