a85cb7c0cc19424a987d5dccb921a8a42a93e429
Daniel Beilinson <beilinson@gmail.com>
2016-03-04 23:58:46 +0300

    Fast snapshots.

--- server/Backend.js
+++ server/Backend.js
@@ -72,7 +72,14 @@ Backend.Prototype = function() {
         self.db.table('changes').insert(record)
           .asCallback(function(err) {
             if (err) return cb(err);
-            cb(null, version);
+            var req = {
+              documentId: args.documentId,
+              version: version
+            };
+            self.requestSnapshotCreation(req, function(err) {
+              if (err) return cb(err);
+              cb(null, version);
+            });
           });
       });
     });
@@ -102,6 +109,7 @@ Backend.Prototype = function() {
         if (err) return cb(err);
         changes = _.map(changes, function(c) {return JSON.parse(c.data); });
         self.getVersion(args.documentId, function(err, headVersion) {
+          if (err) return cb(err);
           var res = {
             currentVersion: headVersion,
             changes: changes
@@ -209,22 +217,26 @@ Backend.Prototype = function() {
 
       var req = {
         documentId: id,
-        sinceVersion: 0
+        sinceVersion: docData.version
       };
-
       self.getChanges(req, function(err, res) {
         if(err) return cb(err);
         
         var docFactory = schemaConfig.documentFactory;
-        var doc = docFactory.createEmptyArticle();
-        
-        _.each(res.changes, function(change) {
-          _.each(change.ops, function(op){
-            doc.data.apply(op);
-          });
-        });
-        
+        var doc = new docFactory.ArticleClass();
         var converter = new JSONConverter();
+        var jsonSnapshot = JSON.parse(docData.snapshot);
+        doc = converter.importDocument(doc, jsonSnapshot);
+        
+        if(res.changes.length > 1) {
+          _.each(res.changes, function(change) {
+            _.each(change.ops, function(op){
+              doc.data.apply(op);
+            });
+          });
+        }
+        
+        
         var output = {
           data: converter.exportDocument(doc),
           version: res.currentVersion,
@@ -254,6 +266,24 @@ Backend.Prototype = function() {
       var result = count[0]['count(*)'];
       return cb(null, result);
     });
+  };
+
+  /*
+    Request creation of new snapshot
+
+    Will create new snapshot only during document
+    creation or in case if version is divisible
+    by frequency constant
+
+    @param {Object} args arguments
+    @param {String} args.documentId document id
+    @param {String} args.version document version from change
+    @param {Function} cb callback
+  */
+  this.requestSnapshotCreation = function(args, cb) {
+    var frequency = this.config.snapshotFrequency;
+    if(args.version % frequency !== 0 && args.version !== 1) return cb(null);
+    this._createSnapshot(args.documentId, args.version, cb);
   };
 
   /*
@@ -329,6 +359,69 @@ Backend.Prototype = function() {
     });
   };
 
+  /*
+    Create snapshot for a given document
+  */
+  this._createSnapshot = function(id, version, cb) {
+    var self = this;
+
+    this._getDocument(id, function(err, docData) {
+      if(err) return cb(err);
+
+      var schemaConfig = self.config.schemas[docData.schemaName];
+      
+      if (!schemaConfig) {
+        cb(new Error('Schema ' + docData.schemaName + ' not found'));
+      }
+
+      var req = {
+        documentId: id,
+        sinceVersion: docData.version || 0
+      };
+
+      self.getChanges(req, function(err, res) {
+        if(err) return cb(err);
+        
+        var doc;
+        var converter = new JSONConverter();
+        var docFactory = schemaConfig.documentFactory;
+
+        if(docData.version > 0) {
+          var jsonSnapshot = JSON.parse(docData.snapshot);
+          doc = new docFactory.ArticleClass();
+          doc = converter.importDocument(doc, jsonSnapshot);
+        } else {
+          doc = docFactory.createEmptyArticle();
+        }
+
+        _.each(res.changes, function(change) {
+          _.each(change.ops, function(op){
+            // doc here should be already restored
+            doc.data.apply(op);
+          });
+        });
+        
+        // doc here should be already restored
+        var snapshot = converter.exportDocument(doc);
+        self._updateSnapshot(id, snapshot, version, cb);
+      });
+    });
+  };
+
+  /*
+    Update document record with new snapshot and version number
+  */
+  this._updateSnapshot = function(id, snapshot, version, cb) {
+    var query = this.db('documents')
+                .where('documentId', id)
+                .update({
+                  snapshot: JSON.stringify(snapshot),
+                  version: version
+                });
+
+    query.asCallback(cb);
+  };
+
   // Users API
   // ---------
 
@@ -386,7 +479,6 @@ Backend.Prototype = function() {
       .asCallback(function(err, userIds) {
         if (err) return cb(err);
         user.userId = userIds[0];
-        //console.log(user.userId, userData.userId);
         cb(null, user);
       });
   };
