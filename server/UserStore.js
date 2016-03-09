1675432edda882b07f55e88b4ea6b88d4322ef19
Daniel Beilinson <beilinson@gmail.com>
2016-03-09 14:13:52 +0300

    UserStore improvements.

--- server/UserStore.js
+++ server/UserStore.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var oo = require('substance/util/oo');
+var Knex = require('knex');
+var knexConfig = require('../knexfile');
+var env = process.env.NODE_ENV || 'development';
 
 /*
   Implements Substance Store API. This is just a stub and is used for
@@ -9,9 +12,28 @@ var oo = require('substance/util/oo');
 function UserStore(config) {
   UserStore.super.apply(this, arguments);
   this.config = config;
+  this.connect();
 }
 
 UserStore.Prototype = function() {
+
+  /*
+    Connect to the db
+  */
+  this.connect = function() {
+    var config = knexConfig[env];
+    if (!config) {
+      throw new Error('Could not find config for environment', env);
+    }
+    this.db = new Knex(config);
+  };
+
+  /*
+    Disconnect from the db and shut down
+  */
+  this.shutdown = function(cb) {
+    this.db.destroy(cb);
+  };
 
   /*
     Create a new user record (aka signup)
@@ -53,6 +75,21 @@ UserStore.Prototype = function() {
   };
 
   /*
+    Get user record for a given loginKey
+  */
+  this._getUserByLoginKey = function(loginKey, cb) {
+    var query = this.db('users')
+                .where('loginKey', loginKey);
+
+    query.asCallback(function(err, user) {
+      if (err) return cb(err);
+      user = user[0]; // query result is an array
+      if (!user) return cb(new Error('Your provided login key was invalid.'));
+      cb(null, user);
+    });
+  };
+
+  /*
     Internal method to create a user entry
   */
   this._createUser = function(userData, cb) {
@@ -71,6 +108,38 @@ UserStore.Prototype = function() {
         user.userId = userIds[0];
         cb(null, user);
       });
+  };
+
+  /*
+    Run migrations
+
+    @param {Function} cb callback
+  */
+  this.runMigration = function(cb) {
+    this.db.migrate.latest({directory: './db/migrations'}).asCallback(function(err){
+      if(err) return cb(err);
+      cb(null);
+    });
+  };
+
+  /*
+    Resets the database and loads a given seed object
+
+    Be careful with running this in production
+
+    @param {Object} seed JSON object
+    @param {Function} cb callback
+  */
+  this.seed = function(seed) {
+    var self = this;
+
+    self.runMigration.call(self, function() {
+      each(seed, function(user){
+        self.createUser(user, function(){
+          console.log('blah');
+        });
+      });
+    });
   };
 
 };
