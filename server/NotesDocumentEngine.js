d7ed43b729538c40e97969509a996f65ec2c25bf
Daniel Beilinson <beilinson@gmail.com>
2016-03-28 15:23:21 +0300

    Enrich get document results.

--- server/NotesDocumentEngine.js
+++ server/NotesDocumentEngine.js
@@ -25,7 +25,48 @@ NotesDocumentEngine.Prototype = function() {
     args.info.title = doc.get(['metadata', 'title']);
     _super.createDocument.call(this, args, cb);
   };
-  
+
+  this.getDocument = function(args, cb) {
+    var self = this;
+    // SQL query powered
+    this.queryDocumentMetaData(args.documentId, function(err, docEntry) {
+      if (err) {
+        return cb(new Err('NotesDocumentEngine.ReadDocumentMetadataError', {
+          cause: err
+        }));
+      }
+      self.snapshotEngine.getSnapshot(args, function(err, snapshot) {
+        if (err) {
+          return cb(new Err('NotesDocumentEngine.ReadSnapshotError', {
+            cause: err
+          }));
+        }
+        docEntry.data = snapshot.data;
+        cb(null, docEntry);
+      });
+    });
+  };
+
+  this.queryDocumentMetaData = function(documentId, cb) {
+    var self = this;
+    this.documentStore.getDocument(documentId, function(err, docEntry) {
+      if (err) {
+        return cb(new Err('NotesDocumentEngine.ReadDocumentError', {
+          cause: err
+        }));
+      }
+      self.changeStore.getCollaborators(docEntry.documentId, docEntry.userId, function(err, collaborators) {
+        if (err) {
+          return cb(new Err('NotesDocumentEngine.ReadDocumentCollaboratorsError', {
+            cause: err
+          }));
+        }
+        docEntry.collaborators = collaborators;
+        cb(null, docEntry);
+      });
+    });
+  };
+
 };
 
 DocumentEngine.extend(NotesDocumentEngine);
