2b6891797aec7115600074641dcf9499b80b9b69
Daniel Beilinson <beilinson@gmail.com>
2016-04-01 02:03:55 +0300

    Expose statistics in cover.

--- client/Cover.js
+++ client/Cover.js
@@ -1,8 +1,11 @@
-  "use strict";
+"use strict";
 
 var Component = require('substance/ui/Component');
 var $$ = require('substance/ui/Component').$$;
 var TextPropertyEditor = require('substance/ui/TextPropertyEditor');
+var Icon = require('substance/ui/FontAwesomeIcon');
+var filter = require('lodash/filter');
+var size = require('lodash/size');
 
 var Cover = function() {
   Cover.super.apply(this, arguments);
@@ -18,8 +21,16 @@ Cover.Prototype = function() {
 
     var authors = [noteInfo.author || noteInfo.userId];
     authors = authors.concat(noteInfo.collaborators.split(','));
+    
     console.log('noteInfo', noteInfo);
+
+    var commentsQt = size(doc.getIndex('type').get('comment'));
+    var commentsLabel = commentsQt == 1 ? 'comment' : 'comments';
+    var issueIndex = doc.getIndex('type').get('todo');
+    var issuesQt = size(issueIndex);
+    var resolvedQt = size(filter(issueIndex, function(i){return i.done;}));
     var metaNode = doc.getDocumentMeta();
+
     return $$("div").addClass("sc-cover")
       .append(
         // Editable title
@@ -32,6 +43,14 @@ Cover.Prototype = function() {
         }).addClass('se-title'),
         $$('div').addClass('se-separator'),
         $$('div').addClass('se-authors').append(authors.join(', ')),
+        $$('div').addClass('se-comments').append(
+          $$(Icon, {icon: "fa-comment-o"}),
+          commentsQt + ' ' + commentsLabel
+        ),
+        $$('div').addClass('se-issues').append(
+          $$(Icon, {icon: "fa-check-square-o"}),
+          resolvedQt + ' of ' + issuesQt
+        ),
         $$('div').addClass('se-authors').append('Updated on ', updatedAt, ' by ', noteInfo.updatedBy)
       );
   };
