28fdf893967b878f3f7986ae314a523ae6dc3bb8
Daniel Beilinson <beilinson@gmail.com>
2016-04-02 02:09:01 +0300

    App bundling.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,61 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
+var sass = require('gulp-sass');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var through2 = require('through2');
+var rename = require('gulp-rename');
+
+/**
+ *  Bundle
+ */
+
+gulp.task('assets', function () {
+  // Index HTML
+  gulp.src('./index.html')
+    .pipe(gulp.dest('./dist'));
+
+  // Assets
+  gulp.src('./styles/assets/**/*')
+    .pipe(gulp.dest('./dist/assets'));
+
+  // Font Awesome
+  gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('sass', function() {
+  gulp.src('./app.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename('app.css'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('browserify', function() {
+  gulp.src('./app.js')
+    .pipe(through2.obj(function (file, enc, next) {
+        browserify(file.path)
+        .bundle(function (err, res) {
+          if (err) { return next(err); }
+          file.contents = res;
+          next(null, file);
+        });
+    }))
+    .on('error', function (error) {
+        console.log(error.stack);
+        this.emit('end');
+    })
+    .pipe(uglify().on('error', function(err){console.log(err); }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('bundle', ['assets', 'sass', 'browserify']);
+
+/**
+ *  Tests
+ */
+
 gulp.task('lint', function() {
   return gulp.src([
     './model/**/*.js',
@@ -17,4 +72,5 @@ gulp.task('test:server', ['lint'], function() {
 });
 
 gulp.task('test', ['lint', 'test:server']);
-gulp.task('default', ['build']);
+
+gulp.task('default', ['bundle']);
