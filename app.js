d0d5546507345d026f18cd0e8b6bf2e14efb1cfc
Michael Aufreiter <michael@substance.io>
2016-04-12 18:32:09 +0200

    Set debug rendering to true.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var Notes = require('./client/Notes');
 var Component = require('substance/ui/Component');
 var $ = window.$ = require('substance/util/jquery');
 
+window.SUBSTANCE_DEBUG_RENDERING = true;
+
 // Start the application
 $(function() {
   window.app = Component.mount(Notes, document.body);
