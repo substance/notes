50478fcf2a81e97ab329b5ad5cd45dc522a9af95
Daniel Beilinson <beilinson@gmail.com>
2016-06-22 00:54:41 +0300

    Note writer package.

--- packages/note-writer/NoteWriter.js
+++ packages/note-writer/NoteWriter.js
@@ -4,6 +4,8 @@ var ProseEditor = require('substance/packages/prose-editor/ProseEditor');
 var ContainerEditor = require('substance/ui/ContainerEditor');
 var SplitPane = require('substance/ui/SplitPane');
 var ScrollPane = require('substance/ui/ScrollPane');
+var Layout = require('substance/ui/Layout');
+var NoteCover = require('../note-cover/NoteCover');
 
 function NoteWriter() {
   NoteWriter.super.apply(this, arguments);
@@ -18,22 +20,34 @@ NoteWriter.Prototype = function() {
 
     return $$('div').addClass('sc-note-writer').append(
       $$(SplitPane, {splitType: 'horizontal'}).append(
-        $$(ToolbarClass, {
-          commandStates: commandStates
-        }).ref('toolbar'),
+        $$('div').addClass('se-toolbar-wrapper').append(
+          $$(Layout, {width: 'large', noPadding: true}).append(
+            $$(ToolbarClass, {
+              commandStates: commandStates
+            }).ref('toolbar')
+          )
+        ),
         $$(ScrollPane, {
           scrollbarType: 'substance',
           scrollbarPosition: 'right',
-          overlay: ProseEditorOverlay,
         }).append(
-          $$(ContainerEditor, {
-            disabled: this.props.disabled,
-            documentSession: this.documentSession,
-            node: this.doc.get('body'),
-            commands: configurator.getSurfaceCommandNames(),
-            textTypes: configurator.getTextTypes()
-          }).ref('body')
-        ).ref('contentPanel')
+          $$(Layout, {
+            width: 'large'
+          }).append(
+            $$(NoteCover, {
+              configurator: configurator,
+              doc: this.doc,
+              noteInfo: this.props.noteInfo
+            }).ref('cover'),
+            $$(ContainerEditor, {
+              disabled: this.props.disabled,
+              documentSession: this.documentSession,
+              node: this.doc.get('body'),
+              commands: configurator.getSurfaceCommandNames(),
+              textTypes: configurator.getTextTypes()
+            }).ref('body')
+          ).ref('contentPanel')
+        )
       )
     );
   };
