210e5f38d90a1a0ca190b4e7163bfc32a348cc68
Daniel Beilinson <beilinson@gmail.com>
2016-06-22 00:57:37 +0300

    Basic comment package,

--- packages/comment/CommentCommand.js
+++ packages/comment/CommentCommand.js
@@ -2,72 +2,7 @@
 
 var AnnotationCommand = require('substance/ui/AnnotationCommand');
 
-var CommentCommand = function(surface) {
-  AnnotationCommand.call(this, surface);
-};
-
-CommentCommand.Prototype = function() {
-
-  this.getSelection = function() {
-    return this.getSurface().getSelection();
-  };
-
-  this.getTargetType = function() {
-    var sel = this.getSelection();
-    if (sel.isNull() || !sel.isPropertySelection()) return null;
-    var doc = this.getDocument();
-    var path = sel.getPath();
-    var node = doc.get(path[0]);
-    // HACK: We should make sure the getCommandState is not called for
-    // an invalid selection.
-    if (!node) return 'paragraph';
-    var nodeType = node.type;
-
-    if (nodeType === 'comment') {
-      return 'paragraph';
-    } else {
-      return 'comment';
-    }
-  };
-
-  this.getCommandState = function() {
-    var surface = this.getSurface();
-    var sel = this.getSelection();
-    var disabled = !surface.isEnabled() || sel.isNull() || !sel.isPropertySelection();
-    var targetType = this.getTargetType();
-
-    return {
-      targetType: targetType,
-      active: targetType !== 'comment',
-      disabled: disabled
-    };
-  };
-
-  // Execute command and trigger transformations
-  this.execute = function() {
-    var sel = this.getSelection();
-    if (!sel.isPropertySelection()) return;
-    var surface = this.getSurface();
-    var targetType = this.getTargetType();
-    var authenticationClient = this.context.authenticationClient;
-    var user = authenticationClient.getUser();
-    if (targetType) {
-      // A Surface transaction performs a sequence of document operations
-      // and also considers the active selection.    
-      surface.transaction(function(tx, args) {
-        args.data = {
-          type: targetType,
-          createdAt: new Date().toISOString(),
-          author: user.name
-        };
-        return surface.switchType(tx, args);
-      });
-      return {status: 'ok'};
-    }
-  };
-};
-
-AnnotationCommand.extend(CommentCommand);
+var CommentCommand = AnnotationCommand.extend();
 
 CommentCommand.static.name = 'comment';
 
