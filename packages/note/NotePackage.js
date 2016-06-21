921633d180e6adb13479292e22a4c06c7bc059be
Daniel Beilinson <beilinson@gmail.com>
2016-06-22 00:56:34 +0300

    Update note package.

--- packages/note/NotePackage.js
+++ packages/note/NotePackage.js
@@ -3,6 +3,20 @@
 var MetaNode = require('./MetaNode');
 var SubstanceNote = require('./SubstanceNote');
 
+var ParagraphPackage = require('substance/packages/paragraph/ParagraphPackage');
+var HeadingPackage = require('substance/packages/heading/HeadingPackage');
+var CodeblockPackage = require('substance/packages/codeblock/CodeblockPackage');
+var BlockquotePackage = require('substance/packages/blockquote/BlockquotePackage');
+var ListPackage = require('substance/packages/list/ListPackage');
+var LinkPackage = require('substance/packages/link/LinkPackage');
+var EmphasisPackage = require('substance/packages/emphasis/EmphasisPackage');
+var StrongPackage = require('substance/packages/strong/StrongPackage');
+var CodePackage = require('substance/packages/code/CodePackage');
+
+var CommentPackage = require('../comment/CommentPackage');
+var MarkPackage = require('../mark/MarkPackage');
+var TodoPackage = require('../todo/TodoPackage');
+
 module.exports = {
   name: 'note',
   configure: function(config) {
@@ -13,5 +27,20 @@ module.exports = {
     });
 
     config.addNode(MetaNode);
+
+    config.import(ParagraphPackage);
+    config.import(HeadingPackage);
+    config.import(CodeblockPackage);
+    config.import(BlockquotePackage);
+    config.import(ListPackage);
+    config.import(EmphasisPackage);
+    config.import(StrongPackage);
+    config.import(CodePackage);
+    config.import(LinkPackage);
+
+    // Import note specific packages
+    config.import(CommentPackage);
+    config.import(MarkPackage);
+    config.import(TodoPackage);
   }
 };
