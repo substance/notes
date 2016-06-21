b7598efed46520db6d1d757546748daa1230d5a4
Daniel Beilinson <beilinson@gmail.com>
2016-06-22 00:53:33 +0300

    Basic todo package.

--- packages/todo/TodoCommand.js
+++ packages/todo/TodoCommand.js
@@ -2,65 +2,7 @@
 
 var AnnotationCommand = require('substance/ui/AnnotationCommand');
 
-var TodoCommand = function(surface) {
-  AnnotationCommand.call(this, surface);
-};
-
-TodoCommand.Prototype = function() {
-
-  this.getCommandState = function() {
-    var surface = this.getSurface();
-    var sel = this.getSelection();
-    var disabled = !surface.isEnabled() || sel.isNull() || !sel.isPropertySelection();
-    var targetType = this.getTargetType();
-
-    return {
-      targetType: targetType,
-      active: targetType !== 'todo',
-      disabled: disabled
-    };
-  };
-
-  // Execute command and trigger transformations
-  this.execute = function() {
-    var sel = this.getSelection();
-    if (!sel.isPropertySelection()) return;
-    var surface = this.getSurface();
-    var targetType = this.getTargetType();
-
-    if (targetType) {
-      // A Surface transaction performs a sequence of document operations
-      // and also considers the active selection.
-      surface.transaction(function(tx, args) {
-        args.data = {
-          type: targetType
-        };
-        return surface.switchType(tx, args);
-      });
-      return {status: 'ok'};
-    }
-  };
-
-  this.getTargetType = function() {
-    var sel = this.getSelection();
-    if (sel.isNull() || !sel.isPropertySelection()) return null;
-    var doc = this.getDocument();
-    var path = sel.getPath();
-    var node = doc.get(path[0]);
-    // HACK: We should make sure the getCommandState is not called for
-    // an invalid selection.
-    if (!node) return 'paragraph';
-    var nodeType = node.type;
-
-    if (nodeType === 'todo') {
-      return 'paragraph';
-    } else {
-      return 'todo';
-    }
-  };
-};
-
-AnnotationCommand.extend(TodoCommand);
+var TodoCommand = AnnotationCommand.extend();
 
 TodoCommand.static.name = 'todo';
 
