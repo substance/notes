8ba894a6262f53111dafcafca83ad462a59f0335
Daniel Beilinson <beilinson@gmail.com>
2016-07-30 02:57:52 +0300

    Notification package.

--- packages/notification/Notification.js
+++ packages/notification/Notification.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Component = require('substance/ui/Component');
+var isUndefined = require('lodash/isUndefined');
 
 function Notification() {
   Component.apply(this, arguments);
@@ -9,6 +10,11 @@ function Notification() {
 Notification.Prototype = function() {
 
   this.render = function($$) {
+    if(isUndefined(this.props.type)) {
+      var emptyEl = $$('div').addClass('sc-empty-notification');
+      return emptyEl;
+    }
+
     var el = $$('div').addClass('sc-notification se-type-' + this.props.type);
     el.append(this.props.message);
     return el;
