82d6e73c9607cd6ec0f7a036f10aa1e57d52e1f8
Daniel Beilinson <beilinson@gmail.com>
2016-07-30 02:54:49 +0300

    Rename packages files.

--- packages/cover/package.js
+++ packages/cover/package.js
@@ -1,8 +1,13 @@
 'use strict';
 
+var Cover = require('./Cover');
+
 module.exports = {
-  name: 'note-cover',
+  name: 'cover',
   configure: function(config) {
+    config.addComponent('cover', Cover);
+    config.addStyle(__dirname, '_cover');
+    config.addStyle(__dirname, '_summary');
     config.addIcon('cover.comments', { 'fontawesome': 'fa-comment-o' });
     config.addIcon('cover.todos', { 'fontawesome': 'fa-check-square-o' });
     config.addIcon('cover.edit', { 'fontawesome': 'fa-pencil' });
--- packages/mark/package.js
+++ packages/mark/package.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 var Mark = require('./Mark');
