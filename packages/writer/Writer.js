c6f3ba55e9fbe3dd1ec30d3919b840c1f1f3e07c
Daniel Beilinson <beilinson@gmail.com>
2016-07-30 02:57:07 +0300

    Writer improvements.

---
+++ packages/writer/Writer.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var ProseEditor = require('substance/packages/prose-editor/ProseEditor');
+var ContainerEditor = require('substance/ui/ContainerEditor');
+var SplitPane = require('substance/ui/SplitPane');
+var ScrollPane = require('substance/ui/ScrollPane');
+var Layout = require('substance/ui/Layout');
+
+function NoteWriter() {
+  NoteWriter.super.apply(this, arguments);
+}
+
+NoteWriter.Prototype = function() {
+
+  this.render = function($$) {
+    var el = $$('div').addClass('sc-document-editor');
+
+    var toolbar = this._renderToolbar($$);
+    var editor = this._renderEditor($$);
+    var cover = this._renderCover($$);
+
+    var contentPanel = $$(ScrollPane, {
+      scrollbarType: 'substance',
+      scrollbarPosition: 'right'
+    }).append(
+      $$(Layout, {
+        width: 'large'
+      }).append(
+        cover,
+        editor
+      )
+    ).ref('contentPanel');
+
+    el.append(
+      $$(SplitPane, {splitType: 'horizontal'}).append(
+        toolbar,
+        contentPanel
+      )
+    );
+    return el;
+  };
+
+  this._renderEditor = function($$) {
+    var configurator = this.props.configurator;
+    return $$(ContainerEditor, {
+      disabled: this.props.disabled,
+      documentSession: this.documentSession,
+      node: this.doc.get('body'),
+      editing: 'full',
+      commands: configurator.getSurfaceCommandNames(),
+      textTypes: configurator.getTextTypes()
+    }).ref('body');
+  };
+
+  this._renderCover = function($$) {
+    var configurator = this.props.configurator;
+    var componentRegistry = this.componentRegistry;
+    var Cover = componentRegistry.get('cover');
+    return $$(Cover, {
+      doc: this.doc,
+      configurator: configurator,
+      mobile: this.props.mobile,
+      editing: 'full',
+      noteInfo: this.props.noteInfo,
+      rubrics: this.props.rubrics
+    }).ref('cover');
+  };
+
+};
+
+ProseEditor.extend(NoteWriter);
+
+module.exports = NoteWriter;
