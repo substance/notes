09371a21184684b499a70588a743e20e78949af2
Daniel Beilinson <beilinson@gmail.com>
2016-08-25 17:40:06 +0300

    Seed document and user stores in snapshot test suite.

--- test/server/SnapshotStore.test.js
+++ test/server/SnapshotStore.test.js
@@ -8,9 +8,54 @@ var snapshotStoreSeed = require('substance/test/fixtures/snapshotStoreSeed');
 var SnapshotStore = require('../../server/SnapshotStore');
 var testSnapshotStore = require('substance/test/collab/testSnapshotStore');
 var snapshotStore = new SnapshotStore({db: db});
+var DocumentStore = require('../../server/DocumentStore');
+var documentStore = new DocumentStore({ db: db });
+var UserStore = require('../../server/UserStore');
+var userStore = new UserStore({ db: db });
 
 function setup() {
   return db.reset()
+    .then(function() {
+      return userStore.seed({
+        'testuser': {
+          userId: 'testuser',
+          name: 'Test',
+          loginKey: '1234',
+          email: 'test@example.com'
+        }
+      });
+    })
+    .then(function() {
+      return documentStore.seed({
+        'test-doc': {
+          documentId: 'test-doc',
+          schemaName: 'prose-article',
+          schemaVersion: '1.0.0',
+          version: 1,
+          info: {
+            userId: 'testuser'
+          }
+        },
+        'test-doc-2': {
+          documentId: 'test-doc-2',
+          schemaName: 'prose-article',
+          schemaVersion: '1.0.0',
+          version: 1,
+          info: {
+            userId: 'testuser'
+          }
+        },
+        'my-doc': {
+          documentId: 'my-doc',
+          schemaName: 'prose-article',
+          schemaVersion: '1.0.0',
+          version: 1,
+          info: {
+            userId: 'testuser'
+          }
+        }
+      });
+    })
     .then(function() {
       var newSnapshotStoreSeed = JSON.parse(JSON.stringify(snapshotStoreSeed));
       return snapshotStore.seed(newSnapshotStoreSeed);
